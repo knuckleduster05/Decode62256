30ce1eda8a99d00b5676cdec0abc6a97bd23f23c
Jack Manning <jmanning@automec.com>
2022-04-12 13:19:44 -0400

    removed fv2 temporarily

--- src/Decode62256Analyzer.cpp
+++ src/Decode62256Analyzer.cpp
@@ -8,7 +8,7 @@ Decode62256Analyzer::Decode62256Analyzer()
 	mSimulationInitilized( false )
 {
 	SetAnalyzerSettings( mSettings.get() );
-	UseFrameV2();
+	//UseFrameV2();
 }
 
 Decode62256Analyzer::~Decode62256Analyzer()
@@ -53,6 +53,7 @@ void Decode62256Analyzer::WorkerThread()
 	currentSample = mCE->GetSampleNumber();
 	
 	for( ; ; ) {
+		//FrameV2 = frame_v2;
 		Frame frame;
 		frame.mFlags = 0;
 		
@@ -91,54 +92,6 @@ void Decode62256Analyzer::WorkerThread()
 		mOE->AdvanceToAbsPosition(currentSample);
 		mCE->AdvanceToAbsPosition(currentSample);
 	}
-	
-	
-/*
-	mSerial = GetAnalyzerChannelData( mSettings->mInputChannel );
-
-	if( mSerial->GetBitState() == BIT_LOW )
-		mSerial->AdvanceToNextEdge();
-
-	U32 samples_per_bit = mSampleRateHz / mSettings->mBitRate;
-	U32 samples_to_first_center_of_first_data_bit = U32( 1.5 * double( mSampleRateHz ) / double( mSettings->mBitRate ) );
-
-	for( ; ; )
-	{
-		U8 data = 0;
-		U8 mask = 1 << 7;
-		
-		mSerial->AdvanceToNextEdge(); //falling edge -- beginning of the start bit
-
-		U64 starting_sample = mSerial->GetSampleNumber();
-
-		mSerial->Advance( samples_to_first_center_of_first_data_bit );
-
-		for( U32 i=0; i<8; i++ )
-		{
-			//let's put a dot exactly where we sample this bit:
-			mResults->AddMarker( mSerial->GetSampleNumber(), AnalyzerResults::Dot, mSettings->mInputChannel );
-
-			if( mSerial->GetBitState() == BIT_HIGH )
-				data |= mask;
-
-			mSerial->Advance( samples_per_bit );
-
-			mask = mask >> 1;
-		}
-
-
-		//we have a byte to save. 
-		Frame frame;
-		frame.mData1 = data;
-		frame.mFlags = 0;
-		frame.mStartingSampleInclusive = starting_sample;
-		frame.mEndingSampleInclusive = mSerial->GetSampleNumber();
-
-		mResults->AddFrame( frame );
-		mResults->CommitResults();
-		ReportProgress( frame.mEndingSampleInclusive );
-	}
-	*/
 }
 
 bool Decode62256Analyzer::NeedsRerun()
